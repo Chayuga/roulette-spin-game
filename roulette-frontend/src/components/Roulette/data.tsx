c22460caccf6e3f56b0f14d214d0d59ce2b675df
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-06-30 23:09:31 +0300

    build: add data file

---
+++ roulette-frontend/src/components/Roulette/data.tsx
@@ -0,0 +1,14 @@
+export const imageDemo = "https://media.istockphoto.com/photos/brown-leather-shoe-picture-id187310279?k=20&m=187310279&s=612x612&w=0&h=WDavpCxsLbj_PRpoY-3PsS2zvuP0Vk0Ci22sRLO9DzE="
+
+export const imageText = "Image Text"
+
+export const discount = 20
+
+export const titleBg = '#149416';
+
+export const titleColor = '#ffffff'
+
+export const itemName = "Leather Shoes"
+
+export const prizeText = `order our ${itemName} with ${discount} discount and indulge in some well deserved self-care while at home!`
+
