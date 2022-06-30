8447943e80577440f0119dd437a7960b28f317a6
Robert Crous <robert.crous21@gmail.com>
2022-06-30 11:12:57 +0200

    chore: refactor class names for Modal Page component

--- roulette-frontend/src/components/Modal/ModalPage.tsx
+++ roulette-frontend/src/components/Modal/ModalPage.tsx
@@ -3,22 +3,22 @@ import './ModalPage.scss'
 
 const ModalPage = ({prizeId, imageDemo, imageText, titleBg, discount, itemName, titleColor, prizeText, closeModal}: any) => {
   return (
-    <main>
-      <div className="modal">
-          <div className="img-container">            
-            <img src={imageDemo} alt={imageText} />  
+      <div className="__mobiz-games-roulette__u-overlay">
+          <div className="__mobiz-games-roulette__modal-page ">
+              <div className="modal-page__header">
+                <img src={imageDemo} alt={imageText} className="modal-page__header__img" />
+              </div>
+            <div className="modal-page__body">
+              <div style={{backgroundColor: titleBg}} className="modal-page__title">
+                <h3 style={{ color: titleColor }} className="modal-page__text-large">{`${ discount}% Discount`}</h3>
+              </div>
+                <p className="modal-page__content">
+                    {prizeText}
+                </p>
+                <button className="modal-page__btn--ok" onClick={closeModal}>Continue</button>
+              </div>
           </div>
-        <div className="text-container">
-          <div style={{backgroundColor: titleBg}} className="title-container">
-            <h3 style={{ color: titleColor }}>{`${ discount}% Discount`}</h3>            
-          </div>
-            <div className="text-content">
-                {prizeText}
-            </div>
-            <button className="btn" onClick={closeModal}>Continue</button>
-          </div>
-      </div> 
-    </main>
+      </div>
     )
 }
 export default ModalPage
