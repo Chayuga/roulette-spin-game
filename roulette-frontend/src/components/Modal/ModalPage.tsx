c06796fb5e41907c0cfc3239d7ad8a4b1bcfb5e7
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-07-01 10:45:24 +0300

    fix: use the right variables from the API

--- roulette-frontend/src/components/Modal/ModalPage.tsx
+++ roulette-frontend/src/components/Modal/ModalPage.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import './ModalPage.scss'
 
-const ModalPage = ({prizeId, imageDemo, imageText, titleBg, discount, itemName, titleColor, prizeText, closeModal}: any) => {
+const ModalPage = ({name, description, additionalInformation, images, bg_colour, text_colour, closeModal}: any) => {
   return (
       <div className="__mobiz-games-roulette__u-overlay">
           <div className="__mobiz-games-roulette__modal-page ">
               <div className="modal-page__header">
-                <img src={imageDemo} alt={imageText} className="modal-page__header__img" />
-              </div>
+                <img src={images} alt="" className="modal-page__header__img" />
+        </div>
+        <h3 className="modal-page__text-large">{`Won! ${description}`}</h3>
             <div className="modal-page__body">
-              <div style={{backgroundColor: titleBg}} className="modal-page__title">
-                <h3 style={{ color: titleColor }} className="modal-page__text-large">{`${ discount}% Discount`}</h3>
+              <div style={{backgroundColor: bg_colour}} className="modal-page__title">
+                <h3 style={{ color: text_colour }} className="modal-page__text-large">{`${ name}% Discount`}</h3>
               </div>
                 <p className="modal-page__content">
-                    {prizeText}
+                    {additionalInformation}
                 </p>
                 <button className="modal-page__btn--ok" onClick={closeModal}>Continue</button>
               </div>
